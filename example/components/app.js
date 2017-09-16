3a1690ef79d329910c767b1ea3454ccf74996915
Quan <lieuchiquan@gmail.com>
2017-09-16 10:37:07 +0700

    Add placeholder image

--- example/components/app.js
+++ example/components/app.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Hello from '../../src';
-import pic from '../images/300.png';
 
 export default class App extends React.PureComponent {
   render() {
     return (
      <div>
-        <Hello><img src={pic} alt=""/></Hello>
+        <Hello>Placeholder</Hello>
       </div>);
   }
 }
