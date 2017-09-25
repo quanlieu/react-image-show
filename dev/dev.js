1a625d4002e0b46188ecb144dcd11fb55ac92b23
Quan <quan.lieu@2359media.com>
2017-09-25 13:42:38 +0700

    Update demo page

--- dev/dev.js
+++ dev/dev.js
@@ -14,12 +14,109 @@ export default class Dev extends React.PureComponent {
   constructor(props) {
     super(props);
     this.images = [m01, m02, m03, m04, m05, m06, m07, m08, m09];
+
+    this.state = {
+      isShowIndicators: true,
+      isShowThumbnails: true,
+      isShowArrows: true,
+      isShowFixedHeight: true
+    }
+
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+  }
+
+  handleCheckbox(e) {
+    const { name } = e.currentTarget;
+    this.setState({[name]: !this.state[name]})
   }
 
   render() {
+    const {
+      isShowIndicators, isShowThumbnails, isShowArrows, isShowFixedHeight
+    } = this.state
+    
+    console.log(isShowFixedHeight);
     return (
       <div>
-        <SlideShow images={this.images} indicators thumbnails fixedImagesHeight />
-      </div>);
+        <h1>Adjust some of these setting</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor="isShowIndicators">Show indicators</label>
+              </td>
+              <td>
+                <input
+                  name="isShowIndicators"
+                  id="isShowIndicators"
+                  type="checkbox"
+                  checked={isShowIndicators}
+                  onChange={this.handleCheckbox}
+                />
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="isShowThumbnails">Show thumbnails</label>
+              </td>
+              <td>
+                <input
+                  name="isShowThumbnails"
+                  id="isShowThumbnails"
+                  type="checkbox"
+                  checked={isShowThumbnails}
+                  onChange={this.handleCheckbox}
+                />
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="isShowArrows">Show arrows</label>
+              </td>
+              <td>
+                <input
+                  name="isShowArrows"
+                  id="isShowArrows"
+                  type="checkbox"
+                  checked={isShowArrows}
+                  onChange={this.handleCheckbox}
+                />
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="isShowFixedHeight">Fixed height</label>
+              </td>
+              <td>
+                <input
+                  name="isShowFixedHeight"
+                  id="isShowFixedHeight"
+                  type="checkbox"
+                  checked={isShowFixedHeight}
+                  onChange={this.handleCheckbox}
+                />
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        {isShowFixedHeight && <SlideShow
+          images={this.images}
+          indicators={isShowIndicators}
+          thumbnails={isShowThumbnails}
+          arrows={isShowArrows}
+          fixedImagesHeight
+        />}
+        {!isShowFixedHeight &&<SlideShow
+          images={this.images}
+          indicators={isShowIndicators}
+          thumbnails={isShowThumbnails}
+          arrows={isShowArrows}
+        />}
+      </div>
+    );
   }
 }
